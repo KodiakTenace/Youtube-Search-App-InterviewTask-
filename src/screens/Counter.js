ba8444233183753fc98d8c6c3c59a7ce998bb0f3
KodiakTenace <mario.k.msan@gmail.com>
2021-01-28 17:08:25 +0200

    set up routing with search and counter screens

---
+++ src/screens/Counter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const Counter = ({}) => {
+  return (
+    <View>
+      <Text>This is counter</Text>
+    </View>
+  );
+};
