c55a8a49d422cc085e438412c2c079d9807b153d
KodiakTenace <mario.k.msan@gmail.com>
2021-01-30 18:40:32 +0200

    add unit test for counter reducer

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,20 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import counterReducer from '../src/redux/reducers/counterReducer';
+import {decrement, increment} from '../src/redux/actions';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+// counter reducer unit testing
+describe('counter reducer', () => {
+  it('should return the initial state', () => {
+    expect(counterReducer(undefined, {})).toEqual(0);
+  });
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+  it('should increment counter', () => {
+    expect(counterReducer(0, increment())).toEqual(1);
+  });
+
+  it('should decrement counter', () => {
+    expect(counterReducer(0, decrement())).toEqual(-1);
+  });
 });
